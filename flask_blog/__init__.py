5833903af502f782781d415cff5e60e16b883182
Ajay Rokkam <ajay.rokkam@mountblue.io>
2018-09-27 15:01:24 +0530

    add a create_app method in __init__.py deviating from migur

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -3,13 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 # inorder to connect with mysql db
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from elasticsearch import Elasticsearch
 
 
 app = Flask(__name__)
+app.config['ELASTICSEARCH_URL'] = 'http://localhost:9200'
+
+
+def create_app():
+    # ...
+    app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+        if app.config['ELASTICSEARCH_URL'] else None
+
 
 
 # random key for security purpose
 app.config['SECRET_KEY'] = '1e9f1b48c53261cf4563d4ea5670657b'
+
+app.config['ELASTICSEARCH_URL'] = 'http://localhost:9200'
 
 # mysqlconnector is necessary it provides a medium of communication between sqlalchemy and mysql db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
